901e6d8c29fa512fe35734fffeb6e4f980af4e3b
Abubakar Salihu <salihuabubakar557@gmail.com>
2024-11-05 12:47:29 +0100

    two mics refactored

--- src/app/_components/ui/two-mic.tsx
+++ src/app/_components/ui/two-mic.tsx
@@ -1,22 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export const TwoMic = () => {
   return (
     <>
-    <div className='empty-banner w-full h-[100px] md:h-[200px]'></div>
-    <div className="flex justify-center items-center w-full relative max-sm:bottom-[100px] bottom-[250px]">
-          <div className="w-5/6 h-auto rounded-2xl overflow-hidden ">
-            <Image
-              src={"/images/two-mic.jpg"}
-              alt="Mic"
-              width={100}
-              height={100}
-              unoptimized
-              className="w-full md:h-[500px] rounded-[40px]"
-            />
-          </div>
+      <div className="empty-banner w-full h-[100px] md:h-[200px]"></div>
+      <div className="flex justify-center items-center w-full relative bottom-[100px] sm:bottom-[300px] md:bottom-[250px]">
+        <div className="w-5/6 h-auto rounded-2xl overflow-hidden ">
+          <Image
+            src={"/images/two-mic.jpg"}
+            alt="Mic"
+            width={100}
+            height={100}
+            unoptimized
+            className="w-full md:h-[500px] rounded-[40px]"
+          />
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
